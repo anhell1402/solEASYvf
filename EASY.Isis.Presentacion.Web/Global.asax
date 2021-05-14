5cd6084c16998d7067514d221c565ad1fba2e082
Angelito <24462@iberoleon.edu.mx>
2021-05-14 14:09:08 -0500

    Agregar archivos de proyecto.

---
+++ EASY.Isis.Presentacion.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EASY.Isis.Presentacion.Web.Global" Language="C#" %>
---
+++ EASY.Isis.Presentacion.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EASY.Isis.Presentacion.Web.Startup))]
+namespace EASY.Isis.Presentacion.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
